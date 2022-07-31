b8303e5900aecb8bf42703fa208db331285a5c6b
omololevy <cotechlevy@gmail.com>
2022-07-31 17:00:27 +0300

    app module for initial imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
+import { AppCommonModule } from "./components/common/app-common.module";
 import { AppComponent } from './app.component';
 
+import { RoutePartsService } from './services/route-parts/route-parts.service';
+import { NgChartsModule } from 'ng2-charts';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n', '.json');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      provide: 
+    }),
+    NgChartsModule
   ],
-  providers: [],
+  providers: [RoutePartsService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
