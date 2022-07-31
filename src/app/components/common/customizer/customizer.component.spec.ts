cbdffcb47d3253dd49715855f468fc8627ef4c31
omololevy <cotechlevy@gmail.com>
2022-07-31 17:12:26 +0300

    customizer  components specs

---
+++ src/app/components/common/customizer/customizer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomizerComponent } from './customizer.component';
+
+describe('CustomizerComponent', () => {
+  let component: CustomizerComponent;
+  let fixture: ComponentFixture<CustomizerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CustomizerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CustomizerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
