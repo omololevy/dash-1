3e6d62c5dddd6a0b202af28b284854db6d6617ed
omololevy <cotechlevy@gmail.com>
2022-08-01 23:42:16 +0300

    Create the home component

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  isAnnualSelected: boolean = false;
+  contactForm: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.email]],
+      subject: ['', [Validators.required]],
+      message: ['', Validators.required]
+    })
+  }
+
+  buyEgret() {
+    window.open('https://themeforest.net/item/egret-angular-4-material-design-admin-template/20161805?ref=mh_rafi');
+  }
+
+}
