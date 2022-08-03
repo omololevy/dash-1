06b802abe7c0b8cc549b4991a27214d8536339d9
omololevy <cotechlevy@gmail.com>
2022-08-03 21:35:22 +0300

    Import dom and services and add the variables

--- src/app/components/common/topbar/topbar.component.ts
+++ src/app/components/common/topbar/topbar.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import * as domHelper from '../../../helpers/dom.helper';
+import { ThemeService } from '../../../services/theme/theme.service';
 
 @Component({
-  selector: 'app-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  selector: 'topbar',
+  templateUrl: './topbar.component.html'
 })
 export class TopbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() sidenav:any;
+  @Input() notificPanel:any;
+  @Output() onLangChange = new EventEmitter<any>();
+  currentLang = 'en';
+  availableLangs = [{
+    name: 'English',
+    code: 'en',
+  }, {
+    name: 'Spanish',
+    code: 'es',
+  }]
+  egretThemes:any;
+  
+  constructor(private themeService: ThemeService) {}
+  ngOnInit() {
+    this.egretThemes = this.themeService.egretThemes;
   }
-
+  setLang() {
+    this.onLangChange.emit(this.currentLang);
+  }
+  changeTheme(theme:any) {
+    this.themeService.changeTheme(theme);
+  }
+  toggleNotific() {
+    this.notificPanel.toggle();
+  }
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+  toggleCollapse() {
+        let appBody = document.body;
+        domHelper.toggleClass(appBody, 'collapsed-menu');
+        domHelper.removeClass(document.getElementsByClassName('has-submenu'), 'open');
+    }
 }
