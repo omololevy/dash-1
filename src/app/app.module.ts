a651e13edad7539174f2e958d166f4851da920c5
omololevy <cotechlevy@gmail.com>
2022-08-03 20:58:23 +0300

    Adjust the chart import and modify the imported and declared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { HttpModule } from '@angular/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { rootRouterConfig } from './app.routes';
 import { AppRoutingModule } from './app-routing.module';
 import { AppCommonModule } from "./components/common/app-common.module";
 import { AppComponent } from './app.component';
 
 import { RoutePartsService } from './services/route-parts/route-parts.service';
-import { NgChartsModule } from 'ng2-charts';
+import { NavigationService } from "./services/navigation/navigation.service";
+import { AuthService } from './services/auth/auth.service';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n', '.json');
@@ -19,12 +22,24 @@ export function createTranslateLoader(http: HttpClient) {
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AppCommonModule,
+    HttpClientModule,
     TranslateModule.forRoot({
-      provide: 
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      }
     }),
-    NgChartsModule
+    RouterModule.forRoot(rootRouterConfig, { useHash: false })
   ],
-  providers: [RoutePartsService,],
+  declarations: [AppComponent],
+  providers: [
+    RoutePartsService,
+    NavigationService,
+    AuthService,
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
