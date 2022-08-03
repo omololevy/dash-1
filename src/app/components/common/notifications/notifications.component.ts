0d82e512574d5f5e5ca382501eefb5b766c29789
omololevy <cotechlevy@gmail.com>
2022-08-03 21:31:03 +0300

    Add imports and the variables to the component

--- src/app/components/common/notifications/notifications.component.ts
+++ src/app/components/common/notifications/notifications.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
+  @Input() notificPanel:any;
 
-  constructor() { }
+  // Dummy notifications
+  notifications = [{
+    message: 'New contact added',
+    icon: 'assignment_ind',
+    time: '1 min ago',
+    route: '/inbox',
+    color: 'primary'
+  }, {
+    message: 'New message',
+    icon: 'chat',
+    time: '4 min ago',
+    route: '/chat',
+    color: 'accent'
+  }, {
+    message: 'Server rebooted',
+    icon: 'settings_backup_restore',
+    time: '12 min ago',
+    route: '/charts',
+    color: 'warn'
+  }]
 
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((routeChange) => {
+        if (routeChange instanceof NavigationEnd) {
+          this.notificPanel.close();
+        }
+    });
   }
-
+  clearAll(e:any) {
+    e.preventDefault();
+    this.notifications = [];
+  }
 }
