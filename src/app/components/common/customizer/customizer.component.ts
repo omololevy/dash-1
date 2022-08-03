d3297828533b2fe4b3190e77099f199fcac6e65b
omololevy <cotechlevy@gmail.com>
2022-08-03 21:26:05 +0300

    Import the services and add the logic of the customizer

--- src/app/components/common/customizer/customizer.component.ts
+++ src/app/components/common/customizer/customizer.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NavigationService } from "../../../services/navigation/navigation.service";
+import * as domHelper from '../../../helpers/dom.helper';
 
 @Component({
   selector: 'app-customizer',
   templateUrl: './customizer.component.html',
-  styleUrls: ['./customizer.component.scss']
+  styleUrls: ['./customizer.component.css']
 })
 export class CustomizerComponent implements OnInit {
+  isCustomizerOpen: boolean = false;
+  selectedMenu: string = 'icon-menu';
+  isBreadcrumbEnabled = true;
+  isTopbarFixed = false;
+  @Input() breadcrumb:any;
+  sidenavTypes = [{
+    name: 'Default Menu',
+    value: 'default-menu'
+  }, {
+    name: 'Separator Menu',
+    value: 'separator-menu'
+  }, {
+    name: 'Icon Menu',
+    value: 'icon-menu'
+  }]
+  constructor(private navService: NavigationService) { }
 
-  constructor() { }
+  ngOnInit() { }
+  changeSidenav(data:any) {
+    this.navService.publishNavigationChange(data.value)
+  }
+  toggleBreadcrumb(data:any) {
+    this.breadcrumb.isEnabled = data.checked;
 
-  ngOnInit(): void {
+    this.toggleTopbarFixed({})
   }
 
+  toggleTopbarFixed(data:any) {
+    if (!this.isTopbarFixed) {
+      this.removeTopbarFixed();
+    } else {
+      if (this.isBreadcrumbEnabled) {
+        this.removeTopbarFixed();
+        domHelper.addClass(document.body, 'fixed-topbar-breadcrumb')
+      } else {
+        this.removeTopbarFixed();
+        domHelper.addClass(document.body, 'fixed-topbar')
+      }
+    }
+  }
+  removeTopbarFixed() {
+    domHelper.removeClass(document.body, 'fixed-topbar');
+    domHelper.removeClass(document.body, 'fixed-topbar-breadcrumb');
+  }
 }
